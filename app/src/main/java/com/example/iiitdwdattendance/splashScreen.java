9f81b32486a7915005d138394d24addc355a14a1
suraj durgesht <surajdurgesht@gmail.com>
2019-08-18 19:35:11 +0530

    upload

---
+++ app/src/main/java/com/example/iiitdwdattendance/splashScreen.java
@@ -0,0 +1,28 @@
+package com.example.iiitdwdattendance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+public class splashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent( splashScreen.this, FullscreenActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
